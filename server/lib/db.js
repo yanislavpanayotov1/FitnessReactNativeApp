689b73ab6d1b6f663810d74cdc2f5227d6dd73ff
Yanislav Panayotov <panayotovyanislav@gmail.com>
2026-01-25 11:38:28 +0200

    db fix

--- server/lib/db.js
+++ server/lib/db.js
@@ -9,7 +9,7 @@ export const db = new Pool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   ssl: { rejectUnauthorized: false }
 });
 
