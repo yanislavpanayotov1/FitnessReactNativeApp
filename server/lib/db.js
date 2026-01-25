3d842e6cc2f7a67208798d9895086f9323c56f66
Yanislav Panayotov <panayotovyanislav@gmail.com>
2026-01-25 11:31:56 +0200

    db fix

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,7 +2,14 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const db = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: process.env.DB_URL && process.env.DB_URL.includes("render") ? { rejectUnauthorized: false } : false
+  // connectionString: process.env.DB_URL,
+  // ssl: process.env.DB_URL && process.env.DB_URL.includes("render") ? { rejectUnauthorized: false } : false 
+
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
